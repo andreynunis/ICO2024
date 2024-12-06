bd5752e0318b9838d4d3c1074cd3f728045943dc
Andrey Nunis <126250249+andreynunis@users.noreply.github.com>
2024-12-05 21:08:19 -0300

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,57 +1,27 @@
 package pacote;
 import robocode.*;
-//import java.awt.Color;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-
-/**
- * Abc - a robot by (your name here)
- */
-public class Abc extends Robot
-{
-	/**
-	 * run: Abc's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(270);
-			back(100);
-			turnGunRight(270);
-		}
-	}
-
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(2);
-	}
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(100);
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(100);
-		// abcbada
-	}	
-}
+    public void run() {
+        setColors(Color.darkGray,Color.black,Color.lightGray,Color.white,Color.green);
+        while (true) {
+            ahead(100);
+            turnRight(90);
+            
+        }
+    }
+    
+    public void onScannedRobot(ScannedRobotEvent e) {
+        String robotank = e.getName();
+        double distancia = e.gettDistance();
+        System.out.println(robotank + " distancia" + distancia);
+        if (distancia < 135) {
+            fire(3);
+        } else {
+            fire(1);
+        }
+    }
+    
+    public void onHitWall(HitWallEvent e) {
+        back 50;
+        turnRight(90);
+    }
