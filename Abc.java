1206858243602c4fc2080ea40cad8714738ce77b
Andrey Nunis <126250249+andreynunis@users.noreply.github.com>
2024-12-10 20:39:08 -0300

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -11,13 +11,26 @@ import robocode.*;
     }
     
     public void onScannedRobot(ScannedRobotEvent e) {
-        String robotank = e.getName();
-        double distancia = e.gettDistance();
-        System.out.println(robotank + " distancia" + distancia);
-        if (distancia < 135) {
-            fire(3);
-        } else {
-            fire(1);
+        double absBearing=e.getBearingRadians()+getHeadingRadians();//enemies absolute bearing
+        double latVel=e.getVelocity() * Math.sin(e.getHeadingRadians() -absBearing);//enemies later velocity
+        double gunTurnAmt;//amount to turn our gun
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//lock on the radar
+        if(Math.random()>.9){
+            setMaxVelocity((12*Math.random())+12);//randomly change speed
+        }
+        if (e.getDistance() > 150) {//if distance is greater than 150
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/22);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt); //turn our gun
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//drive towards the enemies predicted future location
+            setAhead((e.getDistance() - 140)*moveDirection);//move forward
+            setFire(3);//fire
+        }
+        else{//if we are close enough...
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/15);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt);//turn our gun
+            setTurnLeft(-90-e.getBearing()); //turn perpendicular to the enemy
+            setAhead((e.getDistance() - 140)*moveDirection);//move forward
+            setFire(3);//fire
         }
     }
     
